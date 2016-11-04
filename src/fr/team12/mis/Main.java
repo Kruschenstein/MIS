4ad26ae6aa200bc110776dc6076a6f5a2086ad7f
baraline <antoine.guillaume@etu.univ-orleans.fr>
2016-11-04 15:26:58 +0100

    Adding doc to program and handle .graph in program argument

--- src/fr/team12/mis/Main.java
+++ src/fr/team12/mis/Main.java
@@ -2,38 +2,13 @@ package fr.team12.mis;
 
 public class Main
 {
-    private static final String graphResult = "8\n" +
-        "0: [ 1 2 3 4 ]\n" +
-        "1: [ 0 2 5 6 ]\n" +
-        "2: [ 0 1 3 6 ]\n" +
-        "3: [ 0 2 4 7 ]\n" +
-        "4: [ 0 3 5 7 ]\n" +
-        "5: [ 1 4 6 7 ]\n" +
-        "6: [ 1 2 5 7 ]\n" +
-        "7: [ 3 4 5 6 ]\n";
-
-    public static boolean testGraphFactory()
-    {
-        boolean ret = false;
-        Either<Graph, String> result = GraphFactory.generateFromFile(
-            "graphe.graphe");
-
-        if (result.getLeft() != null)
-            System.out.println(result.getLeft());
-
-        else
-        {
-            System.out.println(result.getRight());
-            result.toString().equals(graphResult);
-        }
-        return ret;
-    }
-
     public static void main(String... args)
     {
-        //testGraphFactory();
-        Either<Graph, String> result = GraphFactory.generateFromFile(
-            "graphe.graphe");
+        String filename = "graphe.graphe";
+        if (!args[0].equals("${arg0}"))
+            filename = args[0];
+	
+        Either<Graph, String> result = GraphFactory.generateFromFile(filename);
         
         if (result.getLeft() != null)
         {
@@ -44,10 +19,7 @@ public class Main
 
         Graph graph = result.getRight();
         
-        // System.out.println(graph.findMaxDegreeMaxEdgesVertex());
-        
         System.out.println("graph.MIS():");
         System.out.println(graph.MIS());
-        //System.out.println(graph.getNeighbourEdgeNumber("7"));
     }
 }
